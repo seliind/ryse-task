0d813f6d21247c377ecf3f31ad828078df5ee7c4
Selin <ayseguldemircioglu@outlook.com>
2022-10-23 20:30:00 +0300

    Data from json added to UI

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ const navTexts = document.getElementsByClassName("r-nav-text")
 let isNavBarOpened = false;
 
 arrow.addEventListener("click", () => {
-    if(isNavBarOpened == false) {
+    if (isNavBarOpened === false) {
         navBarContainer.classList.remove("col-1")
         navBarContainer.classList.add("col-2")
         isNavBarOpened = true
@@ -24,33 +24,73 @@ arrow.addEventListener("click", () => {
 })
 
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
+const balance = document.getElementById("balance")
+let clientCount = document.getElementById("client-count")
+const clientPercentage = document.getElementById("client-percentage")
+const invoiceCount = document.getElementById("invoice-count")
+const invoicePercentage = document.getElementById("invoice-percentage")
 
-  const data = {
+const emails = document.getElementById("emails")
+
+
+
+fetch("data/data.json").then(response => response.json())
+    .then(response => {
+        balance.innerHTML = `$${response.balance}`
+        clientCount.innerHTML = `$${response.new_client_count}`
+        clientPercentage.innerHTML = `$${response.new_client_percentage}`
+        invoiceCount.innerHTML = `$${response.invoices_overdue_count}`
+        invoicePercentage.innerHTML = `$${response.invoices_overdue_percentange}`
+        response.emails.forEach(email => {
+            emails.innerHTML += `
+            <div class="row my-2">
+            <div class="card border-0 shadow rounded-4 h-100">
+            <div class="card-body d-flex justify-content-between text-secondary text-sm">
+                <img style="height: 25px; width:25px;" class="rounded"
+                    src="${email.url}" alt="">
+                <p>${email.user}</p>
+                <p>${email.subject}</p>
+                <p>${email.time}</p>
+            </div>
+        </div>
+            `
+        });
+    })
+
+
+const labels = [
+    'February 15',
+    'February 16',
+    'February 17',
+    'February 18',
+    'February 19',
+    'February 20',
+];
+
+const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [10000, 15000, 17523, 20123, 20589, 22300],
     }]
-  };
+};
 
-  const config = {
+const config = {
     type: 'line',
     data: data,
-    options: {}
-  };
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+};
 
 
-  const myChart = new Chart(
+const myChart = new Chart(
     document.getElementById('myChart'),
     config
-  );
+);
