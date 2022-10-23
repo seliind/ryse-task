3b84624102560d63bf251b8610201e31f4f1241f
Selin <ayseguldemircioglu@outlook.com>
2022-10-23 19:52:38 +0300

    Remaining UI Elements added

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,35 @@ arrow.addEventListener("click", () => {
         element.classList.toggle("d-none")
     });
 })
+
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
